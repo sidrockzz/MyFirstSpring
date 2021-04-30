355dff7dcbc5dde1e847a6af3f7de239367dcfaa
sidrockzz <marshtomp.1999@hotmail.com>
2021-04-30 14:17:13 +0530

    Added project

---
+++ src/main/java/com/FirstSpring/MyfirstProject/MyfirstProjectApplication.java
@@ -0,0 +1,13 @@
+package com.FirstSpring.MyfirstProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyfirstProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyfirstProjectApplication.class, args);
+	}
+
+}
